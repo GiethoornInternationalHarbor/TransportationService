82c519219a09d8592e068d34ace090126b5777e5
Marco Havermans <marcohavermans+github@hotmail.nl>
2018-05-22 11:31:04 +0200

    Fixed ack of message when MessageType.Unknown

--- src/infrastructure/rabbitmq/rabbitmq.message.handler.ts
+++ src/infrastructure/rabbitmq/rabbitmq.message.handler.ts
@@ -11,11 +11,12 @@ export class RabbitMQMessageHandler implements IMessageHandler {
   constructor(
     private readonly queue: string,
     private readonly rabbitChannel: RabbitMQChannel
-  ) {}
+  ) { }
 
   public async start(onMessage: IMessageReceivedCallback) {
     return this.rabbitChannel.consume(this.queue, async msg => {
       if (msg == null) {
+        // This should never ever happen ;-)
         return;
       }
 
@@ -24,6 +25,8 @@ export class RabbitMQMessageHandler implements IMessageHandler {
 
       if (receivedType === MessageType.Unknown) {
         // We do not need to handle this, since it is not in our known MessageType values
+        // But we still need to acknowledge the message
+        this.rabbitChannel.ack(msg);
         return;
       }
 
